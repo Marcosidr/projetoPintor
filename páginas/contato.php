746c206c6bc73ef5fea720f5d7c4f2778e5e9d30
Marcos Inácio <marcosincio556@gmail.com>
2025-05-22 20:03:28 -0300

    testando a branch

--- páginas/contato.php
+++ páginas/contato.php
@@ -120,3 +120,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
